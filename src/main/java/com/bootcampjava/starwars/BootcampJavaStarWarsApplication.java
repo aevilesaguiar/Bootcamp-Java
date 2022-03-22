21062eb195cf5ed82839690761308fbfc6f46095
Aevilés Aguiar Silva <52088444+aevilesaguiar@users.noreply.github.com>
2022-03-21 21:06:17 -0300

    criando Jedi

--- src/main/java/com/bootcampjava/starwars/BootcampJavaStarWarsApplication.java
+++ src/main/java/com/bootcampjava/starwars/BootcampJavaStarWarsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BootcampJavaStarWarsApplication {
 
-	public static void main(String[] args) {
+	  public static void main(String[] args) {
 		SpringApplication.run(BootcampJavaStarWarsApplication.class, args);
 	}
 
